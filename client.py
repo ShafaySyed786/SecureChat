8190aafecf6a33651f084a58312d71a4ab9af211
Shafay Syed <ssyed10@calpoly.edu>
2023-07-15 16:12:48 +0300

    idk

--- client.py
+++ client.py
@@ -1,22 +1,51 @@
 import socket
+import threading
+import stun
 
 def start_client():
-    host = input("Enter the server IP: ")  # Get the server IP
-    port = 8000  # Server port
+    server_ip = input("Enter the server IP: ")  # Get the server IP
+    server_port = 8000  # Server port
 
-    s = socket.socket()  # Create a socket object
-    s.connect((host, port))  # Connect to the server
+    local_port = 9000  # Local port for UDP socket
+
+    server_address = (server_ip, server_port)
+    local_address = ('', local_port)
+
+    # Create UDP socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind(local_address)
+
+    print('NAT Type:', stun.get_nat_type(socket=s))
+    print('Public IP:', stun.get_ip_info(socket=s)[0])
+
+    # Send a message to the server to get the peer information
+    s.sendto(b'Hello', server_address)
+
+    # Receive the peer information from the server
+    peer_data, _ = s.recvfrom(1024)
+    peer_ip, peer_port = peer_data.decode().split(',')
+
+    # Start a new thread for receiving messages from the peer
+    receive_thread = threading.Thread(target=receive_messages, args=(s,))
+    receive_thread.start()
+
+    # Connect to the peer
+    peer_address = (peer_ip, int(peer_port))
+    s.sendto(b'Connected', peer_address)
+    print('Connected to peer:', peer_ip, peer_port)
 
     while True:
-        data = input(' -> ')
-        s.send(data.encode())  # Send data to the server.
+        message = input(' -> ')
+        s.sendto(message.encode(), peer_address)  # Send message to the peer.
 
-        data = s.recv(1024).decode()  # Receive data from the server.
+    s.close()  # Close the socket
+
+def receive_messages(s):
+    while True:
+        data, _ = s.recvfrom(1024)  # Receive data from the peer.
         if not data:
             break
-        print('Received from server:', data)
-
-    s.close()  # Close the connection
+        print('Received from peer:', data.decode())
 
 if __name__ == '__main__':
     start_client()
--- server.py
+++ server.py
@@ -1,34 +1,78 @@
 import socket
 import requests
+import threading
 
 def start_server():
-    port = 8000  # Server port
+    server_port = 8000  # Server port
 
-    s = socket.socket()  # Create a socket object
-    s.bind(('', port))  # Bind to the port
+    server_address = ('', server_port)
 
-    s.listen(1)  # Wait for the client connection.
+    # Create UDP socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind(server_address)
+
     print('Server is listening...')
 
-    external_ip = requests.get('https://api.ipify.org').text
-    external_port = port
+    nat_type, external_ip, external_port = get_public_ip_info()
 
+    print('NAT Type:', nat_type)
     print('Public IP:', external_ip)
     print('Public Port:', external_port)
 
-    conn, addr = s.accept()  # Establish a connection with the client.
-    print('Got connection from', addr)
+    peers = {}  # Dictionary to store peer information (IP:Port)
 
     while True:
-        data = conn.recv(1024).decode()  # Receive data from the client.
+        data, address = s.recvfrom(1024)  # Receive data from a client.
+
+        if data == b'Hello':
+            # Get the peer's IP and port
+            peer_ip, peer_port = address
+
+            # Add the peer information to the dictionary
+            peers[peer_ip] = peer_port
+
+            # Send the peer information back to the client
+            s.sendto(f'{peer_ip},{peer_port}'.encode(), address)
+        elif data == b'Connected':
+            # Get the peer's IP and port
+            peer_ip, peer_port = address
+
+            # Find the peer's address based on the stored information
+            for ip, port in peers.items():
+                if ip != peer_ip:
+                    peer_address = (ip, port)
+                    break
+
+            # Start a new thread for communication with the peer
+            communication_thread = threading.Thread(target=handle_communication, args=(s, peer_address, address))
+            communication_thread.start()
+
+def get_public_ip_info():
+    try:
+        response = requests.get('https://api.ipify.org?format=json')
+        data = response.json()
+        external_ip = data['ip']
+        nat_type = 'N/A'
+        external_port = 'N/A'
+    except:
+        nat_type = 'Unknown'
+        external_ip = 'Unknown'
+        external_port = 'Unknown'
+
+    return nat_type, external_ip, external_port
+
+def handle_communication(s, peer_address, client_address):
+    while True:
+        data, _ = s.recvfrom(1024)  # Receive data from a peer.
+
         if not data:
             break
-        print('Received from client:', data)
 
-        data = input(' -> ')
-        conn.send(data.encode())  # Send data to the client.
+        # Send the data to the other peer
+        s.sendto(data, peer_address)
 
-    conn.close()  # Close the connection
+    # Remove the peer's information from the dictionary
+    peers.pop(client_address[0], None)
 
 if __name__ == '__main__':
     start_server()
