088f120ccd17996fc7003b3e9cfdc02001c0b056
Shafay Syed <ssyed10@calpoly.edu>
2023-07-15 16:15:26 +0300

    bruh

--- client.py
+++ client.py
@@ -3,49 +3,28 @@ import threading
 import stun
 
 def start_client():
-    server_ip = input("Enter the server IP: ")  # Get the server IP
-    server_port = 8000  # Server port
+    host = input("Enter the server IP: ")  # Enter the server's IP address
+    port = 8000  # Server port
 
-    local_port = 9000  # Local port for UDP socket
+    s = socket.socket()  # Create a socket object
+    s.connect((host, port))  # Connect to the server
 
-    server_address = (server_ip, server_port)
-    local_address = ('', local_port)
+    nat_type, external_ip, external_port = stun.get_ip_info()
 
-    # Create UDP socket
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.bind(local_address)
-
-    print('NAT Type:', stun.get_nat_type(socket=s))
-    print('Public IP:', stun.get_ip_info(socket=s)[0])
-
-    # Send a message to the server to get the peer information
-    s.sendto(b'Hello', server_address)
-
-    # Receive the peer information from the server
-    peer_data, _ = s.recvfrom(1024)
-    peer_ip, peer_port = peer_data.decode().split(',')
-
-    # Start a new thread for receiving messages from the peer
-    receive_thread = threading.Thread(target=receive_messages, args=(s,))
-    receive_thread.start()
-
-    # Connect to the peer
-    peer_address = (peer_ip, int(peer_port))
-    s.sendto(b'Connected', peer_address)
-    print('Connected to peer:', peer_ip, peer_port)
+    print('NAT Type:', nat_type)
+    print('Public IP:', external_ip)
+    print('Public Port:', external_port)
 
     while True:
-        message = input(' -> ')
-        s.sendto(message.encode(), peer_address)  # Send message to the peer.
+        data = input(' -> ')
+        s.send(data.encode())  # Send data to the server.
 
-    s.close()  # Close the socket
-
-def receive_messages(s):
-    while True:
-        data, _ = s.recvfrom(1024)  # Receive data from the peer.
+        data = s.recv(1024).decode()  # Receive data from the server.
         if not data:
             break
-        print('Received from peer:', data.decode())
+        print('Received from server: ' + data)
+
+    s.close()  # Close the connection
 
 if __name__ == '__main__':
     start_client()
