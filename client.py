dd167b75a2649e2afa0767963efa3d8c86db9088
Shafay Syed <ssyed10@calpoly.edu>
2023-07-15 13:05:33 +0300

    initial base

---
+++ client.py
@@ -0,0 +1,22 @@
+import socket
+
+def start_client():
+    host = 'localhost'  # Host IP
+    port = 8000  # Server port
+
+    s = socket.socket()  # Create a socket object
+    s.connect((host, port))  # Bind to the port
+
+    while True:
+        data = input(' -> ')
+        s.send(data.encode())  # Send data to the server.
+
+        data = s.recv(1024).decode()  # Receive data from the server.
+        if not data:
+            break
+        print('Received from server: ' + data)
+
+    s.close()  # Close the connection
+
+if __name__ == '__main__':
+    start_client()
---
+++ server.py
@@ -0,0 +1,28 @@
+import socket
+
+def start_server():
+    host = 'localhost'  # Host IP
+    port = 8000  # Server port
+
+    s = socket.socket()  # Create a socket object
+    s.bind((host, port))  # Bind to the port
+
+    s.listen(1)  # Wait for the client connection.
+    print('Server is listening...')
+
+    conn, addr = s.accept()  # Establish a connection with the client.
+    print('Got connection from', addr)
+
+    while True:
+        data = conn.recv(1024).decode()  # Receive data from the client.
+        if not data:
+            break
+        print('Received from client: ' + data)
+
+        data = input(' -> ')
+        conn.send(data.encode())  # Send data to the client.
+
+    conn.close()  # Close the connection
+
+if __name__ == '__main__':
+    start_server()
