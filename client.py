31d0b9a53b5bfe2a5c6e40a9c00a01b94e089cbc
ShafaySyed786 <ssyed2004@gmail.com>
2023-07-21 13:59:38 +0300

    updated

--- client.py
+++ client.py
@@ -2,25 +2,40 @@ import socket
 import threading
 
 HOST = '16.171.162.94'  # Replace with the server's public IP address or domain name
-PORT = 5000
+
+# Prompt the user for the port number
+while True:
+    port = input('Enter the port number (between 2500 and 5000): ')
+    try:
+        port = int(port)
+        if 2500 <= port <= 5000:
+            break
+        else:
+            print('Invalid port number. Please enter a value between 2500 and 5000.')
+    except ValueError:
+        print('Invalid port number. Please enter a valid integer value.')
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((HOST, PORT))
+client_socket.connect((HOST, port))
 
 def receive_messages():
     while True:
-        data = client_socket.recv(1024).decode()
-        print('Received message:', data)
+        try:
+            data = client_socket.recv(1024).decode()
+            if not data:
+                break
+            print(data)
+        except:
+            break
 
-receive_thread = threading.Thread(target=receive_messages)
-receive_thread.start()
+threading.Thread(target=receive_messages).start()
+
+# Get the client's name
+name = input('Enter your name: ')
 
 while True:
-    message = input('Enter message (or "exit" to quit): ')
-
+    message = input()
     if message == 'exit':
+        client_socket.sendall('exit'.encode())
         break
-
-    client_socket.sendall(message.encode())
-
-client_socket.close()
+    client_socket.sendall(f'{name}: {message}'.encode())
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 HOST = '0.0.0.0'
 PORT = 5000
@@ -13,19 +14,41 @@ client_sockets = []
 
 def handle_client(client_socket):
     while True:
-        data = client_socket.recv(1024).decode()
+        try:
+            data = client_socket.recv(1024).decode()
 
-        if not data:
+            if not data:
+                break
+
+            # Treat 'exit' as a special message indicating that the client wants to disconnect
+            if data == 'exit':
+                print('Client wants to disconnect')
+                disconnect_msg = "A client has disconnected"
+                notify_all_clients(disconnect_msg, client_socket)
+                break
+
+            print('Received message:', data)
+
+            # Send the message to all connected clients except the sender
+            for socket in client_sockets:
+                if socket != client_socket:
+                    socket.sendall(data.encode())
+        except:
+            # If an exception occurs, it could mean the client has abruptly disconnected
+            print('Client has disconnected')
+            disconnect_msg = "A client has abruptly disconnected"
+            notify_all_clients(disconnect_msg, client_socket)
             break
 
-        print('Received message:', data)
-
-        # Send the message to all connected clients except the sender
-        for socket in client_sockets:
-            if socket != client_socket:
-                socket.sendall(data.encode())
-
+    # Remove the client socket from the list
+    client_sockets.remove(client_socket)
     client_socket.close()
+
+def notify_all_clients(msg, sender_socket):
+    """Helper function to notify all clients about some event."""
+    for socket in client_sockets:
+        if socket != sender_socket:
+            socket.sendall(msg.encode())
 
 while True:
     client_socket, client_address = server_socket.accept()
@@ -34,4 +57,5 @@ while True:
     client_sockets.append(client_socket)
 
     # Start a new thread to handle the client's messages
-    handle_client(client_socket)
+    thread = threading.Thread(target=handle_client, args=(client_socket,))
+    thread.start()
